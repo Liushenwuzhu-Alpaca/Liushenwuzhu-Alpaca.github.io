9b7b05a61162ba6be589313f6e60ba7c42fc0380
Liushenwuzhu-Alpaca <2185099343@qq.com>
2026-08-26 10:10:05 +0800

    feat: add friend 耶克站

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -12,4 +12,10 @@ export const friends: Friend[] = [
     desc: '相寻梦里路，飞雨落花中',
     avatar: 'https://avatars.githubusercontent.com/u/53815918?v=4',
   },
+  {
+    name: '耶克站',
+    url: 'https://brilliant-dango-1b8da7.netlify.app/',
+    desc: '这里是耶克个人站，随便了，反正没人看。',
+    avatar: 'https://brilliant-dango-1b8da7.netlify.app/assets/images/avatar.jpg',
+  },
 ]
